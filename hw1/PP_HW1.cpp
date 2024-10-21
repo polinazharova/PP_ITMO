aff5444002990c769fe80b8fbce4533eb09283a1
polinazharova <1132226165@pfur.ru>
2024-10-22 00:16:23 +0300

    hw1 and hw2 done

---
+++ hw1/PP_HW1.cpp
@@ -0,0 +1,110 @@
+﻿#pragma GCC target("avx2")
+#include <iostream>
+#include <immintrin.h>
+#include <chrono>
+
+void arr_filling(float* arr, size_t n) {
+    for (size_t i{ 0 }; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+}
+
+void arr_printing(float* x_arr, float* y_arr, size_t n) {
+    std::cout << "FIRST:    SECOND: " << std::endl;
+    for (size_t i{ 0 }; i < n; i++) {
+        std::cout << x_arr[i] << "    " << y_arr[i] << std::endl;
+    }
+}
+
+
+float vector(float* x_arr, float* y_arr, size_t n)
+{
+    float s = 0;
+    for (size_t i = 0; i < n; i++)
+        s += x_arr[i] * y_arr[i];
+    return s;
+}
+
+float vector_sse(float* x_arr, float* y_arr, size_t n)
+{
+    float s = 0;
+    float temp_arr[8];
+    size_t i = 0;
+    for (i; i + 8 < n; i += 8) {
+        __m256 x = _mm256_loadu_ps(&x_arr[i]);
+        __m256 y = _mm256_loadu_ps(&y_arr[i]);
+        __m256 res = _mm256_mul_ps(x, y);
+        __m256 temp = _mm256_hadd_ps(res, res); 
+        temp = _mm256_hadd_ps(temp, temp);
+        _mm256_storeu_ps(temp_arr, temp);
+        s += temp_arr[0] + temp_arr[4];
+    }
+    for (; i < n; i++) {
+        s += x_arr[i] * y_arr[i];
+    }
+    
+    return s;
+}
+
+void square(float* init_arr, float* res_arr, size_t n) {
+    for (size_t i{ 0 }; i < n; i++) {
+        res_arr[i] = sqrt(init_arr[i]);
+    }
+}
+
+void square_sse(float* init_arr, float* res_arr, size_t n) {
+    size_t i = 0;
+  
+    for (; i + 8 < n; i += 8) {
+        __m256 init = _mm256_load_ps(&init_arr[i]);
+        __m256 res = _mm256_sqrt_ps(init);
+        _mm256_store_ps(&res_arr[i], res);
+    }
+
+    for (; i < n; i++) {
+        res_arr[i] = sqrt(init_arr[i]);
+    }
+}
+
+int main() {
+    std::cout << "N: ";
+    size_t N; std::cin >> N;
+
+    alignas(32) float* x = new float[N];
+    alignas(32) float* y = new float[N];
+
+    arr_filling(x, N);
+
+    std::cout << "SSE SQUARE:" << std::endl;
+    auto start_time = std::chrono::high_resolution_clock::now();
+    square_sse(x, y, N);
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+    std::cout << "TIME: " << time.count() << std::endl;
+  
+    std::cout << "STANDART SQUARE:" << std::endl;
+    start_time = std::chrono::high_resolution_clock::now();
+    square(x, y, N);
+    end_time = std::chrono::high_resolution_clock::now();
+    time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+    std::cout << "TIME: " << time.count() << std::endl;
+
+    std::cout << "SSE VECTOR:" << std::endl;
+    start_time = std::chrono::high_resolution_clock::now();
+    std::cout << vector_sse(x, y, N) << std::endl;
+    end_time = std::chrono::high_resolution_clock::now();
+    time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+    std::cout << "TIME: " << time.count() << std::endl;
+
+    std::cout << "STANDART VECTOR:" << std::endl;
+    start_time = std::chrono::high_resolution_clock::now();
+    std::cout << vector(x, y, N) << std::endl;
+    end_time = std::chrono::high_resolution_clock::now();
+    time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+    std::cout << "TIME: " << time.count() << std::endl;
+
+    delete[] x;
+    delete[] y;
+
+    return 0;
+}
---
+++ hw2/hw2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <random>
+
+
+struct ThreadParamsForMatrix
+{
+    int* arr_str;
+    size_t str_size;
+    long long str_odd_mult;
+};
+
+DWORD WINAPI str_generating(LPVOID lpParam) {
+    std::mt19937 gen(static_cast<unsigned int>(GetCurrentThreadId()));
+    std::uniform_int_distribution<> dis(0, 99);
+
+    ThreadParamsForMatrix* params = static_cast<ThreadParamsForMatrix*>(lpParam);
+    long long mult = 1;
+    for (size_t i{ 0 }; i < params->str_size; i++) {
+        (params->arr_str)[i] = dis(gen);
+        if ((i + 1) % 2 != 0) {
+            mult *= (params->arr_str)[i];
+        }
+    }
+    return static_cast<DWORD>(mult);
+}
+
+void str_printing(ThreadParamsForMatrix* params) {
+    std::cout << "MULTIPLICATION: " << params->str_odd_mult << std::endl;
+    for (size_t i{ 0 }; i < params->str_size; i++) {
+        std::cout << (params->arr_str)[i] << "|";
+    }
+    std::cout << std::endl << std::endl;
+}
+
+void matrix_deleting(int** matrix, size_t n) {
+    for (size_t i{ 0 }; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+int main()
+{
+    std::cout << "n and m: ";
+    size_t n, m; std::cin >> n >> m;
+
+    int** matrix = new int*[n];
+    ThreadParamsForMatrix* params = new ThreadParamsForMatrix{NULL, m, NULL};
+  
+    for (int i{ 0 }; i < n; i++) {
+        matrix[i] = new int[m];
+        params->arr_str = matrix[i];
+        HANDLE thread = CreateThread(NULL, 0, str_generating, params, 0, NULL);
+        if (thread) {
+            WaitForSingleObject(thread, INFINITE);
+            DWORD exitCode;
+            if (GetExitCodeThread(thread, &exitCode)) {
+                params->str_odd_mult = static_cast<long long>(exitCode);
+            }
+            else {
+                std::cerr << "Failed to get exit code." << std::endl;
+                return -1;
+            }
+            CloseHandle(thread);
+        }
+        else {
+            std::cerr << "Failed to create thread." << std::endl;
+            return -1;
+        }
+        str_printing(params);
+    }
+
+    matrix_deleting(matrix, n);
+    delete params;
+
+    return 0;
+}
+
